d32269777aefd1a888c484e394f4004fb4d47090
if09325 <51119971+if09325@users.noreply.github.com>
2020-04-21 09:50:25 +0200

    Add files via upload

---
+++ reg_lin.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#library pandas to read files
+import pandas as pd 
+import matplotlib.pyplot as plt 
+from scipy import stats
+
+
+#load my data inside a data frame 
+startups_df = pd.read_csv(r"ds.csv")
+
+#select X variables (independant)
+X_RD_Spend = startups_df.iloc[0:len(startups_df),0]
+X_Administration_Spend = startups_df.iloc[0:len(startups_df),1]            
+X_MarketingSpend = startups_df.iloc[0:len(startups_df),2]
+#select Y variables (dependent) 
+Y_Profit = startups_df.iloc[0:len(startups_df),4]
+                     
+
+slope, intercept, r_value, p_value,std_err = stats.linregress(X_RD_Spend,Y_Profit)
+def predict(x) : 
+    return (slope * x + intercept)
+#draw a graph using payplot including FitLine(ligne des prédictions)
+fitLine = predict(X_RD_Spend)
+plt.plot(X_RD_Spend,fitLine,c='red')
+axes = plt.axes()
+axes.grid()
+plt.scatter(X_RD_Spend,Y_Profit)
+
+#plt.scatter(X_Administration_Spend, Y_Profit)
+#plt.scatter(X_MarketingSpend, Y_Profit)
+plt.show()
+                     
+
